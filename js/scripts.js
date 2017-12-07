440e6ca559a37423e134cb31d38ce96a20d72c04
Amelia Jacobs and Julian Alexander <julian@>
2017-12-06 19:03:22 -0800

    Make forms disappear after use

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 //Back End
-function Player(name, roll, turnTotal, gameTotal) {
+function Player(name, roll, turnTotal, gameTotal, myTurn) {
   this.name = name;
   this.roll = roll;
   this.turnTotal = turnTotal;
   this.gameTotal = gameTotal;
+  this.myTurn = myTurn;
 }
 
 Player.prototype.randomNum = function() {
@@ -12,17 +13,18 @@ Player.prototype.randomNum = function() {
   return num;
 }
 
-// Player.prototype.hold = function(result) {
-//   this.gameTotal += this.turnTotal;
-//   this.turnTotal = 0;
-//   this.roll = 0;
-//   player1Turn = !player1Turn;
-// }
+Player.prototype.hold = function(result) {
+  this.gameTotal += this.turnTotal;
+  this.turnTotal = 0;
+  this.roll = 0;
+  player1Turn = !player1Turn;
+}
 
-Player.prototype.addRoll = function() {
+Player.prototype.addRoll = function(player1Turn) {
   if (this.roll === 1) {
     console.log("sky");
     this.turnTotal = 0;
+    this.myTurn = !this.myTurn;
   } else {
     this.turnTotal += this.roll;
     console.log("tar");
@@ -33,37 +35,38 @@ Player.prototype.addRoll = function() {
 $(document).ready(function() {
 
   // var turnTotal = 0;
-  var newPlayer1 = new Player(name, 0, 0, 0);
-  var newPlayer2 = new Player(name, 0, 0, 0);
+  var newPlayer1 = new Player(name, 0, 0, 0, true);
+  var newPlayer2 = new Player(name, 0, 0, 0, false);
   var player1Turn = true;
 
-
+//Player 1 Disply Function
   $('#player1form').submit(function(event) {
     event.preventDefault();
     var player1Name = $("input#player1").val();
+    $("#player1form").hide();
 
     $("#name1").text(player1Name);
   });
-
+//Player 2 Disply Function
   $('#player2form').submit(function(event) {
     event.preventDefault();
     var player2Name = $("input#player2").val();
-
+    $("#player2form").hide();
     $("#name2").text(player2Name);
   });
 
   $('#roll').click(function(){
     if (player1Turn === true) {
       newPlayer1.roll = newPlayer1.randomNum();
-      newPlayer1.addRoll();
+      newPlayer1.addRoll(player1Turn);
       console.log(newPlayer1);
     };
-    // result = this.randomNum();
-    // $("#result").text(result);
-    // turnTotal = turnTotal + result;
+
+    $("#result").text(newPlayer1.roll);
     $("#turn-total").text(newPlayer1.turnTotal);
+    console.log(player1Turn);
   });
   $('#hold').click(function(){
-    console.log("this");
+
   });
 });
